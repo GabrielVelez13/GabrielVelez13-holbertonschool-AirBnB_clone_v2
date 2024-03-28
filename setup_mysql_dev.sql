bc329bfa6007ffe0d1c34491a82494a3faa31439
Brook <christianlopezrivera0@gmail.com>
2024-03-27 23:42:54 -0400

    pushing sql servers prototype

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,13 @@
+-- Create database and user for project
+
+-- Create or use hbnb_dev_db database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create or update user hbnb_dev
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to hbnb_dev on hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
---
+++ setup_mysql_test.sql
@@ -0,0 +1,13 @@
+-- Prepare MySQL server for project
+
+-- Create or use hbnb_test_db database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create or update user hbnb_test
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant privileges to hbnb_test on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
