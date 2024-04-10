2fe4f9190edad5e3f4fb7bfbe6ffa3c9aabc2537
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-10 14:10:51 -0400

    adding docs

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
 from flask import Flask
+
+"""  Create an instance of the Flask class """
 app = Flask(__name__)
 
+""" Define a route for the root URL ("/") and disable strict slashes """
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    This function is the route handler for the root URL ("/").
+    It returns the string "Hello HBNB!" as the response.
+    """
     return "Hello HBNB!"
 
+""" Define a route for the "/hbnb" URL and disable strict slashes """
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    This function is the route handler for the "/hbnb" URL.
+    It returns the string "HBNB" as the response.
+    """
     return "HBNB"
 
+""" Run the Flask application if this file is executed directly """
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
